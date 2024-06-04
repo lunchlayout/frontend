fb91839f40bfba902728c803f6f3be64490516d8
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-06-05 00:00:27 +0700

    edited getOriginURL

--- src/entities/Dish/ui/index.ts
+++ src/entities/Dish/ui/index.ts
@@ -4,7 +4,6 @@ import DishList from "./DishList";
 import DishQuiz from "./DishQuiz";
 import DishStory from "./DishStory";
 import DishVideo from "./DishVideo";
-import DishLoadedPanel from "./DishLoadedPanel";
 import DishInfoModal from "./DishInfoModal";
 import EntertainmentSlider from "./EntertainmentSlider";
 import DishModel3D from "./DishModel3D";
@@ -17,7 +16,6 @@ export {
 	DishQuiz,
 	DishStory,
 	DishVideo,
-	DishLoadedPanel,
 	DishInfoModal,
 	EntertainmentSlider,
 	DishModel3D,
